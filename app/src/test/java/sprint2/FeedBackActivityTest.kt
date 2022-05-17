af1c5054df45eaff9b95166ccc9b5e9d39891700
Xarlifue360 <xarlifue360@gmail.com>
2022-05-17 19:38:23 +0200

    prueba testing

---
+++ app/src/test/java/sprint2/FeedBackActivityTest.kt
@@ -0,0 +1,14 @@
+package sprint2
+
+import org.junit.Assert
+import org.junit.Test
+
+class FeedBackActivityTest {
+
+    @Test
+    fun verificateFeedback() {
+        feedback = nombreClase
+        assertEquals("mensaje esperado", feedback.)
+    }
+
+}
